1ff33973ca9c11e110a624003af94cc7d42d77ce
ZahraAstrom <zahraabdo1@hotmail.com>
2020-03-24 19:39:36 -0500

    loading images as buttons

--- flashcards/src/main/java/zee/project/flashcards/App.java
+++ flashcards/src/main/java/zee/project/flashcards/App.java
@@ -1,5 +1,14 @@
 package zee.project.flashcards;
 
+import java.awt.BorderLayout;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.imageio.*;
+import javax.swing.*;
+
 /**
  * Hello world!
  */
@@ -7,11 +16,29 @@ public final class App {
     private App() {
     }
 
+    private File getFileFromResources(String filename) {
+        ClassLoader classLoader = getClass().getClassLoader();
+        URL resource = classLoader.getResource(filename);
+        return new File(resource.getFile());
+    }
     /**
      * Says hello to the world.
+     * 
      * @param args The arguments of the program.
+     * @throws IOException
      */
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        App app = new App();
+        JFrame frame = new JFrame("My First GUI");
+        JButton kitBtn = new JButton();
+        BufferedImage kitten = ImageIO.read(app.getFileFromResources("puppy.jpg"));
+        kitBtn.setIcon(new ImageIcon(kitten));
+
+       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       frame.setSize(300,300);
+       JButton button = new JButton("Press");
+       frame.getContentPane().add(BorderLayout.CENTER, kitBtn);
+       frame.getContentPane().add(BorderLayout.SOUTH, button); // Adds Button to content pane of frame
+       frame.setVisible(true);
     }
 }
